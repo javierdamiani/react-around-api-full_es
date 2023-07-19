13952103fcc35e67daee1908550c0cf617d00ec7
Javier Damiani <javierdamiani74@gmail.com>
2023-07-19 16:41:08 -0500

    update the api_base_url

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,2 +1,1 @@
-// export const API_BASE_URL = "https://api.javierdamiani.chickenkiller.com";
-export const API_BASE_URL = "http://localhost:3000";
+export const API_BASE_URL = "https://api.javierdamiani.chickenkiller.com";
